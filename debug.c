8a11eeaf8cc65f39b5d9ca857c99bfff16719f89
ahjf <5515159@gmail.com>
2022-07-21 18:34:38 +0800

    add local variables support from chapter 22

--- debug.c
+++ debug.c
@@ -5,6 +5,7 @@
 
 static int simpleInstruction(const char* name, int offset);
 static int constantInstruction(const char* name, Chunk* chunk, int offset);
+static int byteInstruction(const char * name, Chunk * chunk, int offset);
 
 void disassembleChunk(Chunk* chunk, const char* name) {
     printf("==%s==\n", name);
@@ -29,61 +30,46 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     {
         case OP_CONSTANT:
             return constantInstruction("OP_CONSTANT", chunk, offset);
-
         case OP_NIL:
             return simpleInstruction("OP_NIL", offset);
-
         case OP_TRUE:
             return simpleInstruction("OP_TRUE", offset);
-
         case OP_FALSE:
             return simpleInstruction("OP_FALSE", offset);
-
         case OP_POP:
             return simpleInstruction("OP_POP", offset);
-
+        case OP_GET_LOCAL:
+            return byteInstruction("OP_GET_LOCAL", chunk, offset);
+        case OP_SET_LOCAL:
+            return byteInstruction("OP_SET_LOCAL", chunk, offset);
         case OP_DEFINE_GLOBAL:
             return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
-
         case OP_GET_GLOBAL:
             return constantInstruction("OP_GET_GLOBAL", chunk, offset);
-
         case OP_SET_GLOBAL:
             return constantInstruction("OP_SET_GLOBAL", chunk, offset);
-
         case OP_EQUAL:
             return simpleInstruction("OP_EQUAL", offset);
-
         case OP_GREATER:
             return simpleInstruction("OP_GREATER", offset);
-
         case OP_LESS:
             return simpleInstruction("OP_LESS", offset);
-
         case OP_ADD:
             return simpleInstruction("OP_ADD", offset);
-
         case OP_SUBTRACT:
             return simpleInstruction("OP_SUBTRACT", offset);
-
         case OP_MULTIPLY:
             return simpleInstruction("OP_MULTIPLY", offset);
-
         case OP_DIVIDE:
             return simpleInstruction("OP_DIVIDE", offset);
-
         case OP_NOT:
             return simpleInstruction("OP_NOT", offset);
-
         case OP_NEGATE:
             return simpleInstruction("OP_NEGATE", offset);
-
         case OP_PRINT:
             return simpleInstruction("OP_PRINT", offset);
-
         case OP_RETURN:
             return simpleInstruction("OP_RETURN", offset);
-
         default:
             printf("Unknown opcode %d\n", instruction);
             return offset+1;
@@ -93,6 +79,12 @@ int disassembleInstruction(Chunk* chunk, int offset) {
 static int simpleInstruction(const char* name, int offset) {
     printf("%s\n", name);
     return offset+1;
+}
+
+static int byteInstruction(const char * name, Chunk * chunk, int offset) {
+    uint8_t slot = chunk->code[offset+1];
+    printf("%-16s %4d\n", name, slot);
+    return offset+2;
 }
 
 static int constantInstruction(const char* name, Chunk* chunk, int offset) {
