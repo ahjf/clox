b2c8e51652d6c6a23859977c407683482fd89686
ahjf <5515159@gmail.com>
2022-07-26 16:11:11 +0800

    support for functions part 1 from chapter 24

--- vm.h
+++ vm.h
@@ -4,16 +4,22 @@
 
 #ifndef CLOX_VM_H
 #define CLOX_VM_H
-
+#include "object.h"
 #include "chunk.h"
 #include "value.h"
 #include "table.h"
+#define FRAMES_MAX 64
+#define STACK_MAX (FRAMES_MAX * UINT8_COUNT)
 
-#define STACK_MAX 256
+typedef struct  {
+    ObjFunction * function;
+    uint8_t * ip;
+    Value * slots;
+} CallFrame;
 
 typedef struct {
-    Chunk* chunk;
-    uint8_t* ip;
+    CallFrame frames[FRAMES_MAX];
+    int frameCount;
     Value stack[STACK_MAX];
     Value* stackTop;
     Table globals;
