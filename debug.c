cebc2c1661ce461bc3255752fb057ea50773cfde
ahjf <5515159@gmail.com>
2022-08-11 19:25:15 +0800

    add class methods and initializers support from chapter 28

--- debug.c
+++ debug.c
@@ -8,6 +8,7 @@ static int simpleInstruction(const char* name, int offset);
 static int constantInstruction(const char* name, Chunk* chunk, int offset);
 static int byteInstruction(const char * name, Chunk * chunk, int offset);
 static int jumpInstruction(const char * name, int sign, Chunk * chunk, int offset);
+static int invokeInstruction(const char * name, Chunk * chunk, int offset);
 
 void disassembleChunk(Chunk* chunk, const char* name) {
     printf("==%s==\n", name);
@@ -86,6 +87,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return jumpInstruction("OP_LOOP", -1, chunk, offset);
         case OP_CALL:
             return byteInstruction("OP_CALL", chunk, offset);
+        case OP_INVOKE:
+            return invokeInstruction("OP_INVOKE", chunk, offset);
         case OP_CLOSURE: {
             offset++;
             uint8_t constant = chunk->code[offset++];
@@ -107,6 +110,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return simpleInstruction("OP_RETURN", offset);
         case OP_CLASS:
             return constantInstruction("OP_CLASS", chunk, offset);
+        case OP_METHOD:
+            return constantInstruction("OP_METHOD", chunk, offset);
         default:
             printf("Unknown opcode %d\n", instruction);
             return offset+1;
@@ -137,4 +142,13 @@ static int constantInstruction(const char* name, Chunk* chunk, int offset) {
     printValue(chunk->constants.values[constant]);
     printf("'\n");
     return offset+2;
+}
+
+int invokeInstruction(const char *name, Chunk *chunk, int offset) {
+    uint8_t constant = chunk->code[offset + 1];
+    uint8_t argCount = chunk->code[offset + 2];
+    printf("%-16s (%d args) %4d '", name, argCount, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("\n");
+    return offset + 3;
 }
